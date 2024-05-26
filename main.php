264026449055895ed0ec24b955a5fd85ebb83d7e
nazeer311 <169006840+nazeer311@users.noreply.github.com>
2024-05-26 21:27:59 +0530

    travel

---
+++ main.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Travel Website</title>
+    <link rel="stylesheet" href="main.css" />
+  </head>
+  <body bgcolor="#EOEOEO">
+    <header>
+      <header>
+        <div class="logo">
+          <a href="#"><img src="images/imagekgf.jpg" alt="logo" /></a>
+        </div>
+        <nav class="nav-menu">
+          <ul>
+            <li><a href="homepage.php">Home</a></li>
+            <li><a href="#destinations">Destinations</a></li>
+            <li><a href="#hotels">Hotels</a></li>
+            <li><a href="#activites">Activities</a></li>
+            <li><a href="#contact">Contact</a></li>
+             <li><a href="#about">About</a></li>
+          </ul>
+        </nav>
+      </header>
+    </header>
+    <main>
+      <section class="hero">
+        
+        <video autoplay loop muted plays inline class="back-video">
+          <source src="images/hero(1).mp4" type="video/mp4">
+        </video>
+        </div>
+        <div class="back-video-content">
+          <h1>Welcome to KGF </h1>
+          <p>Discover amazing destinations.</p>
+        </div>
+      </section> 
+       <!-- Destinations -->
+       <section id="destinations" class="destinations">
+        <h2>Popular Destination</h2>
+        <div class="destination-grid">
+          <div class="destination-item">
+            <img src="images/Cyanide.jpg" alt="" />
+            <h3>Cyanide</h3>
+            <a href="cynade.html">MORE</a>
+            <p>
+                Visitors can now have access to the mining pits and the cyanide hill, which is a mound of waste deposited after the extraction of gold. 
+                Bharat Gold Mining Limited (BGML) shut operations here in 2001.
+                The two parts of the movie were shot at the dust dump and this is the spot where most of the visitors are attracted to.
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/dam1.avif" alt="" />
+            <h3>Dam</h3>
+            <a href="dam.html">MORE</a>
+            <p>
+                The town is centred around a large man-made lake created by damming the Palar river. 
+                The lake supplies drinking water to the nearby mining city of Kolar Gold Fields ("KGF").
+                 The Bethamangala water works was established in 1903-04 and is a popular tourist site.
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/davastana.jpg" alt="" />
+            <h3>Kotilingeshwara</h3>
+            <a href="Kotilingeshwara.html">MORE</a>
+            <p>
+                ‘Koti’ in Kannada means a crore and Kotilingeshwara is 1 crore Shivalingas.
+              and is installed here along with 90+ lakhs of other Shivalingas of different sizes.
+              In 1980, the temple was initiated and constructed by Swami Sambha Shiva Murthy.
+               The road to the temple is easily approachable and is located in Kammasanadra, a small village in Kolar District.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="destinations">
+        <div class="destination-grid">
+          <div class="destination-item">
+            <img src="images/kaigal.jpeg" alt="" />
+            <h3>Kaigal Falls</h3>
+            <a href="kaigal.html">MORE</a>
+            <p>
+              The falls are mainly formed by the Kaigal Stream, one of the top flowing streams running through the Koundinya Wildlife Sanctuary. The magnificent beauty of the waterfall increases with the presence of the colourful birds, shrubs,
+               wildlife Sanctuary and plenty full of trees spread all across.
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/gidigi1" alt="" />
+            <h3>Gidigi Falls</h3>
+            <a href="gidigi.html">MORE</a>
+            <p>
+              It was a very wide waterfalls with a short fall. We tried to reach the base but due to the extreme rains and overflow of water we just visited it from a safe distance and safety always comes first in any of our trips..
+               The locals said the falls can be reached the base and played in water during post monsoon season.</p>
+          </div>
+          <div class="destination-item">
+            <img src="images/mallapa.jpg" alt="" />
+            <h3>Mallappa Konda</h3>
+            <a href="mallappa konda.html">MORE</a>
+            <p>
+              Mallappa Konda in Karnataka is a serene hill offering a spiritual retreat.
+               The climb unveils breathtaking vistas, rewarding effort with tranquility.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section id="hotels" class="destinations">
+        <h2>Featured Hotels</h2>
+        <div class="destination-grid">
+          <div class="destination-item">
+            <img src="images/resort" alt="" />
+            <h3>The Silvan Villa Resort</h3>
+            <a href="Silvan Villa.html">MORE</a>
+            <p>
+              A place to which people frequently or generally go for relaxation or pleasure,
+                 especially one providing rest and recreation facilities for vacationers
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/juice.JPG" alt="" />
+            <h3>Juice Magic</h3>
+            <a href="juice magic.html">MORE</a>
+            <p>
+              This juicer from Juice magic is Multi - funtion manual juicer. 
+              It is Made up of high quality material at par with food safety standards.
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/surabhi.jpg" alt="" />
+            <h3>SURABHI GRAND</h3>
+            <a href="surabhi.html">MORE</a>
+            <p>
+              surabhi grand provides a flavorful and satisfying dining experience that celebrates the abundance and diversity of plant-based ingredients while promoting health, sustainability, and compassion.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section id="activites" class="destinations">
+        <h2>Featured Activities</h2>
+        <div class="destination-grid">
+          <div class="destination-item">
+            <img src="images/sar fantasy.jpg" alt="" />
+            <h3>Sar Fantasy</h3>
+            <a href="sar fanstasy.html">MORE</a>
+            <p>
+              Sar Fantasy is a park that features various attractions, such as rides and games, as well as other events for entertainment purposes. A theme park is a type of amusement park that bases its structures and attractions around a central theme, 
+              often featuring multiple areas with different themes.
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/gaming.jpg" alt="" />
+            <h3>Sky Gaming Cafe</h3>
+            <a href="gaming.html">MORE</a>
+            <p>
+              A Gaming café (also known as a cyber café) is a café(or a convenience store or a fully dedicated Game-access business) that provides Games access to the public. The fee for using a console or pc is generally charged as a time-based rate.
+            </p>
+          </div>
+          <div class="destination-item">
+            <img src="images/air4.png" alt="" />
+            <h3>The Little England</h3>
+            <a href="Air rifle.html">MORE</a>
+            <p>
+              The objective of the course is to provide awareness and knowledge about the sport shooting to civilians. 
+              The course consist of 6 classes each session for 2 hrs duration. The course teach about the safe handling of shooting equipment and taking care of the sports equipment. It is also an introductory step to learn about various techniques of shooting, rules to be obeyed for safe gun handling, etc. This course has been made mandatory by the Commissioner of Police, Bangalore, for procurement of Arms License. 
+              On the completion of the course a certificate is given to students.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section id="about" class="about">
+        <h3>About Us</h3>
+        <div class="team-members">
+          <div class="team-member">
+            <img src="images/sbmjc.jpg" alt="" />
+            <h4>Sir BhagaWan Mahaveer Jain Collage</h4>
+            <p>Dr. Rekha Sethi
+              Principal - SBMJC-KGF</p>
+            <p>SBMJC as the 21st constituent in the JGI group was established in the year 2004 and till date has
+               crossed several barriers effectively on its way to impart quality education with un disputable physical infrastructure and learning models.</p>
+          </div>
+        <div class="team-members">
+            <div class="team-member">
+              <img src="images/faizan.jpg" alt="" />
+              <h4>FAIZAN.N</h4>
+              <p></p>
+            </div>
+            <div class="team-member">
+              <img src="images/syed.jpg" alt="" />
+              <h4>SYED NAZEER AHMED</h4>
+              <p></p>
+            </div>
+            <div class="team-member">
+              <img src="images/team-member-3.jpg" alt="" />
+              <h4>MOHAMMED WASIM</h4>
+              <p></p>
+            </div>
+          </div>
+        </section>
+        <section id="contact" class="contact">
+          <h3>Contact Us</h3>
+          <div class="contact-info">
+            <p>
+              If you have any questions or would like to book a trip, please fill
+              out the form below or contact us using the information provided.
+            </p>
+            <ul>
+              <li>
+                <i class="fas fa-map-marker-alt"></i>Geetha Road, Robertsonpet, K.G.F-563112
+              </li>
+              <li>
+                <i class="fas fa-envelope"></i
+                ><a href="mailto:info@travelcompany.com"
+                  >faizanfaizan3706@gmail.com</a
+                >
+              </li>
+              <li>
+                <i class="fas fa-phone-alt"></i
+                ><a href="tel:555-123-4567">97416-04610</a>
+              </li>
+            </ul>
+          </div>
+          <form action="https://api.web3forms.com/submit" method="POST" class="form">
+            <div class="form-group">
+              <input type="hidden" name="access_key" value="1d525499-d6ab-458c-bd6f-9294d8f6d824" value="63036e84-7f64-4896-b8f6-4f08dffb8012">
+                <input
+                name="name"
+                name="name"
+                id="name"
+                placeholder="Enter Your Name"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter Your Email"
+              />
+            </div>
+            <div class="form-group">
+              <textarea
+                name="textarea"
+                id="textarea"
+                cols="30"
+                rows="10"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <button type="submit">Send Message</button>
+          </form>
+        </section>
+        <footer>
+          <div class="social-icons">
+            <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+          </div>
+          <p>&copy; 2023 Your Travel Website. All Rights Reserved.</p>
+        </footer>
+      </main>
+    </body>
+    <script
+      src="https://kit.fontawesome.com/6558443b63.js"
+      crossorigin="anonymous"
+    ></script>
+  </html>
+         
